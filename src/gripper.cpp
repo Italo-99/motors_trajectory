ed9a29f6962d676dc5c3865c06a5de6f238b5169
Italo <255778@studenti.unimore.it>
2024-10-05 20:32:32 +0200

    Update repo

--- src/gripper.cpp
+++ src/gripper.cpp
@@ -31,7 +31,7 @@
  *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  *
- *  Author: [Andrea Pupa] [Italo Almirante]
+ *  Author: [Italo Almirante]
  *  Created on: [2024-01-17]
 */
 
